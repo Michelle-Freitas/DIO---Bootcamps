940e526fea2d3134dbb42247b62fb45fe90ac64a
Michelle Freitas <michelle.afreitas7@gmail.com>
2023-11-27 20:16:22 -0300

    memory-game

---
+++ 2. potencia-tech-Ifood-games-DIO/2.memoria/README.md
@@ -0,0 +1,8 @@
+# Jogo da Memória
+### DIO - Bootcamp - PotenciaTech Ifood
+
+Jogo desenvolvido no Bootcamp da DIO - PotenciaTech Ifood Desenvolvimento de jogos.
+Objetivo encontrar os pares iguais de cada carta.
+
+### Imagem do projeto
+![Jogo da memória](public/memo.png)
---
+++ 2. potencia-tech-Ifood-games-DIO/2.memoria/src/scripts/main.js
@@ -0,0 +1,45 @@
+const emojis = ["💜", "💜", "😍", "😍", "😂", "😂", "👌", "👌", "😎", "😎", "🤦‍♀️", "🤦‍♀️", "😢", "😢", "✌️", "✌️"]
+let openCards = []
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1)) //aletoarizar
+
+const resetBtn = document.querySelector('.reset-btn')
+
+function handleClick() {
+    if(openCards.length < 2 ){
+        this.classList.add('box-open')
+        openCards.push(this)
+    }
+
+    if(openCards.length === 2 ){
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add('box-match')
+        openCards[1].classList.add('box-match')
+    } else {
+        openCards[0].classList.remove('box-open')
+        openCards[1].classList.remove('box-open')
+    }
+    openCards = []
+
+    if(document.querySelectorAll('.box-match').length === emojis.length) {
+        alert('VOCÊ VENCEU!!')
+    }
+}
+
+resetBtn.addEventListener('click', () => window.location.reload())
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item'
+    box.innerHTML = shuffleEmojis[i]
+    box.onclick = handleClick
+
+    document.querySelector('.game').appendChild(box)
+}
+
+
